548a00ad11c9bb7746390a7414b4baecd03273de
aziz <mohamedaziz2234@gmail.com>
2023-08-27 11:40:09 +0100

    authentification + reset

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SigninComponent } from './components/signin/signin.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignupComponent } from './components/signup/signup.component';
+import { RecoverPasswordComponent } from './components/recover-password/recover-password.component';
 
 
 
 @NgModule({
   declarations: [
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    RecoverPasswordComponent,
   ],
   imports: [ CommonModule,AuthRoutingModule],
   providers: [],
