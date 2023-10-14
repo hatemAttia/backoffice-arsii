d0058f449b286645efb7a87b7c331cebbe3e2c92
hatem attia <h.attia@teamdev.tn3>
2023-10-14 15:10:31 +0100

    fix list post

--- src/app/private/components/user-list/add-user/add-user.component.ts
+++ src/app/private/components/user-list/add-user/add-user.component.ts
@@ -4,36 +4,31 @@ import { User } from 'interfaces/user';
 import { UserService } from '../userService/user.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-
-
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.scss']
+  styleUrls: ['./add-user.component.scss'],
 })
 export class AddUserComponent implements OnInit {
-
-  constructor(private router: Router, private userService: UserService) { }
+  constructor(private router: Router, private userService: UserService) {}
   userForm!: FormGroup;
-
 
   newUser: any = {};
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   onSubmit(userData: any) {
-    this.userService.addUser(userData).subscribe((response) => {
-      console.log('response', response);
-      if(response) {
-        this.router.navigate(['/private/user-list']);
-      }
-    })
+    this.userService
+      .addUser({ ...userData, userName: userData.email })
+      .subscribe((response) => {
+        console.log('response', response);
+        if (response) {
+          this.router.navigate(['/private/user-list']);
+        }
+      });
   }
 
   goBack() {
     this.router.navigate(['/private/user-list']);
   }
-
 }
